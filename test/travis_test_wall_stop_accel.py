6641833b25dbde4d5a6a4bd551288ffa8607eeff
Katsuya Nakahira <ba2vwbus13wind@gmail.com>
2019-08-06 01:18:49 +0000

    add wall trace

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -18,7 +18,7 @@ class WallStopTest(unittest.TestCase):
 		left, right = self.set_and_get(400,100,100,0)
 		self.assertTrue(left == right == 0, "can't stop (600)")
 
-#		left, right = self.set_and_get(40,0,0,9)
+		left, right = self.set_and_get(40,0,0,9)
 #		self.assertTrue(0 < left == right < 1000, "can't move again")
 
 #		time.sleep(5.0)
